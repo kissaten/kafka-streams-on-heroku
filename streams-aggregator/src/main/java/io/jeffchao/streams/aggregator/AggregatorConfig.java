4150ac59b489c84c145db088cde31f44ad62935b
Jeff Chao <jeffchao@me.com>
2017-11-07 22:21:07 -0800

    Connect to Kafka.

---
+++ streams-aggregator/src/main/java/io/jeffchao/streams/aggregator/AggregatorConfig.java
@@ -0,0 +1,118 @@
+package io.jeffchao.streams.aggregator;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.List;
+import java.util.Properties;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.heroku.sdk.EnvKeyStore;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class AggregatorConfig extends Properties {
+
+  private static final Logger log = LoggerFactory.getLogger(AggregatorConfig.class);
+
+  private static final String SUFFIX = "_JCHAO";
+  private static final String HEROKU_KAFKA = String.format("HEROKU_KAFKA%s", SUFFIX);
+  private static final String HEROKU_KAFKA_URL = String.format("%s_URL", HEROKU_KAFKA);
+  private static final String HEROKU_KAFKA_TRUSTED_CERT =
+      String.format("%s_TRUSTED_CERT", HEROKU_KAFKA);
+  private static final String HEROKU_KAFKA_CLIENT_CERT_KEY =
+      String.format("%s_CLIENT_CERT_KEY", HEROKU_KAFKA);
+  private static final String HEROKU_KAFKA_CLIENT_CERT =
+      String.format("%s_CLIENT_CERT", HEROKU_KAFKA);
+
+  private String bootstrapServers;
+
+  public Properties getProperties() throws URISyntaxException, CertificateException,
+      NoSuchAlgorithmException, KeyStoreException, IOException {
+    return buildDefaults();
+  }
+
+  private Properties buildDefaults() throws CertificateException, NoSuchAlgorithmException,
+      KeyStoreException, IOException, URISyntaxException {
+    Properties defaultProperties = new Properties();
+    Properties herokuKafkaConfigVarProperties = buildHerokuKafkaConfigVars();
+    Properties kafkaStreamsProperties = buildKafkaStreamsDefaults();
+
+    defaultProperties.putAll(herokuKafkaConfigVarProperties);
+    defaultProperties.putAll(kafkaStreamsProperties);
+
+
+    return defaultProperties;
+  }
+
+  private Properties buildHerokuKafkaConfigVars() throws URISyntaxException, CertificateException,
+      NoSuchAlgorithmException, KeyStoreException, IOException {
+    Properties properties = new Properties();
+    List<String> bootstrapServerList = Lists.newArrayList();
+
+    Iterable<String> kafkaUrl = Splitter.on(",")
+        .split(Preconditions.checkNotNull(System.getenv(HEROKU_KAFKA_URL)));
+
+    for (String url : kafkaUrl) {
+      URI uri = new URI(url);
+      bootstrapServerList.add(String.format("%s:%d", uri.getHost(), uri.getPort()));
+
+      switch (uri.getScheme()) {
+      case "kafka":
+        properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT");
+        break;
+      case "kafka+ssl":
+        properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        EnvKeyStore envTrustStore = EnvKeyStore.createWithRandomPassword(HEROKU_KAFKA_TRUSTED_CERT);
+        EnvKeyStore envKeyStore = EnvKeyStore.createWithRandomPassword(
+            HEROKU_KAFKA_CLIENT_CERT_KEY, HEROKU_KAFKA_CLIENT_CERT);
+
+        File trustStoreFile = envTrustStore.storeTemp();
+        File keyStoreFile = envKeyStore.storeTemp();
+
+        properties.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, envTrustStore.type());
+        properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, trustStoreFile.getAbsolutePath());
+        properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, envTrustStore.password());
+        properties.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, envKeyStore.type());
+        properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, keyStoreFile.getAbsolutePath());
+        properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, envKeyStore.password());
+        break;
+      default:
+        throw new URISyntaxException(uri.getScheme(), "Unknown URI scheme");
+      }
+    }
+
+    bootstrapServers = Joiner.on(",").join(bootstrapServerList);
+
+    return properties;
+  }
+
+  private Properties buildKafkaStreamsDefaults() {
+    Properties properties = new Properties();
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
+    properties.put(StreamsConfig.CLIENT_ID_CONFIG, "client");
+    properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    properties.put(
+        StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+        Serdes.String().getClass().getName());
+    properties.put(
+        StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+        Serdes.String().getClass().getName());
+    properties.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+    properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+
+    return properties;
+  }
+}
--- streams-aggregator/src/main/java/io/jeffchao/streams/aggregator/WordCount.java
+++ streams-aggregator/src/main/java/io/jeffchao/streams/aggregator/WordCount.java
@@ -1,7 +1,11 @@
 package io.jeffchao.streams.aggregator;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
@@ -9,38 +13,21 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class WordCount {
 
-  public static void main(String[] args) {
-    final String bootstrapServers =
-        Optional.ofNullable(System.getenv("HEROKU_KAFKA_JCHAO_NAVY"))
-            .orElse("localhost:9092");
-    final Properties streamsConfiguration = new Properties();
-    // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
-    // against which the application is run.
-    streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "app");
-    streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "client");
-    // Where to find Kafka broker(s).
-    streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    // Specify default (de)serializers for record keys and for record values.
-    streamsConfiguration.put(
-        StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-        Serdes.String().getClass().getName());
-    streamsConfiguration.put(
-        StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-        Serdes.String().getClass().getName());
-    // Records should be flushed every 10 seconds. This is less than the default
-    // in order to keep this example interactive.
-    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-    // For illustrative purposes we disable record caches
-    streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+  private static final Logger log = LoggerFactory.getLogger(WordCount.class);
+
+  public static void main(String[] args) throws CertificateException, NoSuchAlgorithmException,
+      KeyStoreException, IOException, URISyntaxException {
+    Properties streamsConfig = new AggregatorConfig().getProperties();
 
     final Serde<String> stringSerde = Serdes.String();
     final Serde<Long> longSerde = Serdes.Long();
@@ -58,7 +45,7 @@ public class WordCount {
 
     wordCounts.toStream().to("wordswithcounts", Produced.with(stringSerde, longSerde));
 
-    final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+    final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfig);
 
     streams.cleanUp();
     streams.start();
