74743c716615ac3e5266c376d530d1c79382768d
Jeff Chao <jeffchao@me.com>
2017-12-16 11:25:40 -0800

    Rename application.id.

--- streams-anomaly-detector/src/main/java/io/jeffchao/streams/anomalydetector/AnomalyDetectorConfig.java
+++ streams-anomaly-detector/src/main/java/io/jeffchao/streams/anomalydetector/AnomalyDetectorConfig.java
@@ -109,9 +109,9 @@ class AnomalyDetectorConfig extends Properties {
   private Properties buildKafkaStreamsDefaults() {
     Properties properties = new Properties();
     properties.put(StreamsConfig.APPLICATION_ID_CONFIG,
-        String.format("%sanomaly-checker-app", HEROKU_KAFKA_PREFIX));
+        String.format("%sanomaly-detector-app", HEROKU_KAFKA_PREFIX));
     properties.put(StreamsConfig.CLIENT_ID_CONFIG,
-        String.format("%sanomaly-checker-client", HEROKU_KAFKA_PREFIX));
+        String.format("%sanomaly-detector-client", HEROKU_KAFKA_PREFIX));
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     properties.put(
         StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
